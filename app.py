e23f3e3d577f8967ff1f28a5458807a2c1877869
4ditya08 <115731151+4ditya08@users.noreply.github.com>
2024-07-22 08:58:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
-
+OPENAI_API_KEY = "sk-proj-i9DWHH9cdSOq5tXsv0KdT3BlbkFJ9zhUAVevUhMd2SESjC1c"
 # Load environment variables
 load_dotenv()
 
